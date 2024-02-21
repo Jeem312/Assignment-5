5c9110d11675feef249cd7ec33edfaa83abe4d35
Jeem312 <Shajidajeem312@gmail.com>
2024-02-21 18:56:19 +0600

    adding all functions

--- ticketing.js
+++ ticketing.js
@@ -6,9 +6,12 @@ var paribahanSection = document.getElementById('paribahan-section');
 
 const buttons = document.querySelectorAll('.button');
 let clickCount = 0;
+let array = []
 for(const button of buttons){
     button.addEventListener('click', function(){
       
+      
+
         clickCount++
         if(clickCount <= 4){
 
@@ -38,15 +41,18 @@ for(const button of buttons){
             emptyDiv.appendChild(listIteam1);
             emptyDiv.appendChild(listIteam2);
             emptyDiv.appendChild(listIteam3);
-            
-       
 
-
+            const totalCost = getTextAndConvertParseIntElementById('total-price');
+            const totalPrice = totalCost + 550;
+            setInnerTextById('total-price', totalPrice);  
+    
 
            }
            else{
             alert('You have selected maximum limit')
            }
+       
+    
           
     })}
     function hideInput(){
@@ -54,6 +60,21 @@ for(const button of buttons){
         
         if(couponHide === 'NEW15' || couponHide === 'Couple 20' ) {
             hideId('input-div')
+            
+            if( couponHide === 'NEW15'){
+                const costTotal = getTextAndConvertParseIntElementById('total-price');
+            const CouponPrice = (costTotal * 15) / 100;
+            const grandPrice = costTotal - CouponPrice; 
+
+            setInnerTextById('grand-price',  grandPrice) 
+            } 
+            else{   const costTotal = getTextAndConvertParseIntElementById('total-price');
+            const CouponPrice = (costTotal * 20) / 100;
+            const grandPrice = costTotal - CouponPrice; 
+
+            setInnerTextById('grand-price',  grandPrice) 
+
+            }
         }
     else{
         alert('provide right coupon code')
